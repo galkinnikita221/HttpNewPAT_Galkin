0f0a04ba1411e3886cdc49c7e32780301cdc8240
Nikita <galkinnikita228@gmail.com>
2024-12-22 19:12:02 +0500

    Получение информации с сайта. Galkin

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,17 @@ namespace HttpNewPAT_Galkin
     {
         static void Main(string[] args)
         {
+            WebRequest request = WebRequest.Create("http://news.permaviat.ru/main");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine(response.GetResponseStream());
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            Console.Read();
         }
     }
 }
